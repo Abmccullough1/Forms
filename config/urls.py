c5c15dac81ee5a25ace5078175bbade1e73aae8e
Abmccullough1 <wmccullough24@basecampcodingacademy.com>
2023-11-29 08:31:21 -0600

    done now?

--- config/urls.py
+++ config/urls.py
@@ -4,13 +4,13 @@ from django.urls import path
 from app.views import *
 
 urlpatterns = [
-    path('no-teen', render_no_teen, name="no-teen"),
-    path("xyz", xyz_there, name="xyz"),
-    path("font-time", render_font_time, name="font-time"),
-    path("centered",centered_average, name="average"),
+    path('logic-2/no-teen-sum/', render_no_teen, name="no-teen"),
+    path("string-2/xyz-there/", xyz_there, name="xyz"),
+    path("warmup-2/font-times/", render_font_time, name="font-time"),
+    path("list-2/centered-average/",centered_average, name="average"),
     path('', main_page, name="main"), 
-    path('how_old', render_old, name="old"),
-    path('order', render_order, name="order"),
+    path('functions/how_old', render_old, name="old"),
+    path('functions/order', render_order, name="order"),
     path('functions/hey_you', hey_you_page ,name="hey"),
     path('codingbat', codingbat_page, name="codingbat"),
     path('functions', function_page, name="function"),
